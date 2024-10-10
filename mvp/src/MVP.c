2a40c553098f9e15697d03ed410bb9ec46c94b7f
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-10-10 14:10:09 -0300

    :sparkles: feat: adds files to mvp
    - test environment with mvp
    
    	by @LilianMS

--- mvp/src/MVP.c
+++ mvp/src/MVP.c
@@ -6,56 +6,49 @@
 /*   By: lilmende <lilmende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:39:16 by lsampiet          #+#    #+#             */
-/*   Updated: 2024/10/10 13:29:14 by lilmende         ###   ########.fr       */
+/*   Updated: 2024/10/10 14:08:00 by lilmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <readline/readline.h> //adicionar a flag -lreadline ao comando de compilação.
-#include <readline/history.h>
-#include <sys/types.h> // tipo pid_t
-#include <sys/wait.h>
-
 int main() {
-    char *line;
+	char    *line;
 
-    while (1) {
-        // Exibe o prompt e lê a linha de comando
-        line = readline("minishell> ");
-        
-        // Adiciona a linha ao histórico de comandos
-        add_history(line);
-        
-        // Verifica se o comando não é vazio
-        if (line && *line) {
-            // Quebra o comando em argumentos (simplesmente por espaço aqui)
-            char *args[] = {line, NULL};
-            
-            // Cria um processo filho para executar o comando
-            pid_t pid = fork();
-            if (pid == 0) {
-                // Processo filho: executa o comando
-                if (execve(args[0], args, NULL) == -1) {
-                    perror("minishell");
-                }
-                exit(EXIT_FAILURE);
-            } else if (pid > 0) {
-                // Processo pai: espera o processo filho terminar
-                wait(NULL);
-            } else {
-                perror("fork");
-            }
-        }
-        // Libera a memória alocada pela função readline
-        free(line);
-    }
+	while (1) {
+		line = readline("minishell> ");
+		if (!line)
+		{
+			ft_putstr_fd("exit\n", STDOUT_FILENO);
+			break ;
+		}
+		add_history(line);
+		if (*line) {
+			
+			char *args[] = {line, NULL};
+			
+			// Cria um processo filho para executar o comando
+			pid_t pid = fork();
+			if (pid == 0) {
+				// Processo filho: executa o comando
+				if (execve(args[0], args, NULL) == -1) {
+					perror("minishell");
+				}
+				exit(EXIT_FAILURE);
+			} else if (pid > 0) {
+				// Processo pai: espera o processo filho terminar
+				wait(NULL);
+			} else {
+				perror("fork");
+			}
+		}
+		free(line);
+	}
 
-    return 0;
+	return 0;
 }
 
-// Para compilar: gcc -o minishell minishell.c -lreadline
+// a readline inicia o prompt "minishell> " e lê a linha de comando.
+// Se CTRL+D for pressionado, o programa exibirá "exit" e sairá do loop.
+// Se a linha de comando não for vazia, ela é adicionada ao histórico de comandos.
+// A linha de comando é quebrada em argumentos (simplesmente por espaço aqui).
