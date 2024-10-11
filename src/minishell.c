cdd96cc0284ba905216f42349486c77d2b90b814
Letícia <lesampietro@gmail.com>
2024-10-10 23:42:03 -0300

    :boom: fix: não tínhamos dado git push nessas alterações

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 12:38:49 by lsampiet          #+#    #+#             */
-/*   Updated: 2024/10/10 16:29:59 by lsampiet         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:41:39 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	lexical_analysis(char *line)
 	if(!m_check_quotes(line))
 	{
 		ft_putstr_fd("Missing closing quote", 2);
+		free(line);
+		clear_history();
 		exit(EXIT_FAILURE);
 	}
 }
@@ -28,13 +30,17 @@ void	lexical_analysis(char *line)
 
 int	main(void)
 {
-	t_mini	*mini;
 	char *line;
 
-	mini = NULL;
 	while (1)
 	{
 		line = readline("minishell> ");
+		if (!line)
+		{
+			ft_putstr_fd("exit\n", STDOUT_FILENO);
+			clear_history();
+			break ;
+		}
 		// init_minishell(mini, line);
 		add_history(line);
 		lexical_analysis(line);
