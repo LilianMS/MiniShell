bf0985430104bd105f0e3ea2fac3a4f6f9314038
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-10-16 11:57:53 -0300

    :pencil: draft: edit test main; implement quotes...
    - edit test main;
    - implement quotes and $

--- mvp/src/main_ras_token_copy.c
+++ mvp/src/main_ras_token_copy.c
@@ -6,7 +6,7 @@
 /*   By: lilmende <lilmende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 09:17:16 by lilmende          #+#    #+#             */
-/*   Updated: 2024/10/16 11:56:16 by lilmende         ###   ########.fr       */
+/*   Updated: 2024/10/16 00:22:55 by lilmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,88 +93,20 @@ void	m_process_operator(char *input, int *i, t_token **tokens)
 	}
 }
 
-// auxiliares para process_word
-char	*m_handle_quotes(char *input, int *i)
-{
-	char	quote;
-	int		start;
-	
-	quote = input[*i];
-	start = ++(*i);  // Avança após a aspa
-	while (input[*i] && input[*i] != quote)
-		(*i)++;
-	if (input[*i] == quote)  // Fecha a aspa
-		(*i)++;
-	return (ft_strndup(&input[start], (*i) - start - 1));  // Retorna o conteúdo entre aspas
-}
-
-char	*m_handle_expansion(char *input, int *i)
-{
-	char	*var_name;
-	char	*var_value;
-	int		start;
-	
-	start = ++(*i); // Avança após o '$'
-	while (input[*i] && (ft_isalnum(input[*i]) || input[*i] == '_'))
-		(*i)++;
-	var_name = ft_strndup(&input[start], (*i) - start);
-	var_value = getenv(var_name); // Obtém o valor da variável de ambiente
-	free(var_name);
-	if (var_value)
-		return (ft_strdup(var_value));
-	else
-		return (ft_strdup("")); // Retorna string vazia
-}
-
-void process_word(char *input, int *i, t_token **tokens)
+void	m_process_word(char *input, int *i, t_token **tokens)
 {
 	char	*word;
-	char	*quoted_word;
-	char	*expanded_word;
 	int		start;
 
-	word = NULL;
 	start = *i;
 	while (input[*i] && !ft_isspace(input[*i]) && !m_is_special_char(input[*i]))
-	{
-		if (input[*i] == '\'' || input[*i] == '\"')
-		{
-			// Copia a parte antes das aspas
-			if (*i > start)
-				word = ft_strndup(&input[start], *i - start);
-			// Trata o conteúdo dentro das aspas
-			quoted_word = m_handle_quotes(input, i);
-			word = ft_realloc(word, ft_strlen(word) + ft_strlen(quoted_word) + 1);
-			ft_strcat(word, quoted_word);
-			free(quoted_word);
-			start = *i;  // Atualiza o início após as aspas
-		}
-		else if (input[*i] == '$')
-		{
-			// Copia a parte antes da expansão
-			if (*i > start)
-				word = ft_strndup(&input[start], *i - start);
-			// Expande a variável
-			expanded_word = m_handle_expansion(input, i);
-			word = ft_realloc(word, ft_strlen(word) + ft_strlen(expanded_word) + 1);
-			ft_strcat(word, expanded_word);
-			free(expanded_word);
-			start = *i;  // Atualiza o início após a expansão
-		}
-		else
-			(*i)++;  // Continua avançando
-	}
-	if (start < *i) {
-		char *remaining_word = ft_strndup(&input[start], *i - start);
-		word = ft_realloc(word, ft_strlen(word) + ft_strlen(remaining_word) + 1);
-		ft_strcat(word, remaining_word);
-		free(remaining_word);
-	}
-	m_add_token(tokens, m_create_token(word, CMD));  // Ajuste conforme a lógica de comando/argumento
+		(*i)++;
+	word = ft_strndup(&input[start], *i - start);
+	m_add_token(tokens, m_create_token(word, CMD)); // Resolver: diferenciar comando e argumento
 	free(word);
 }
 
-t_token *m_tokenize(char *input)
+t_token	*m_tokenize(char *input)
 {
 	t_token	*tokens;
 	int		i;
@@ -184,12 +116,12 @@ t_token *m_tokenize(char *input)
 	while (input[i])
 	{
 		m_skip_whitespace(input, &i);
-		if (m_is_special_char(input[i]))
+		if (input[i] == '|' || input[i] == '>' || input[i] == '<')
 			m_process_operator(input, &i, &tokens);
 		else if (input[i])
 			m_process_word(input, &i, &tokens);
 	}
-	return tokens;
+	return (tokens);
 }
 
 /*
