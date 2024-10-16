8529a18f7c2de2f61e624e1169bccb5fd336f7a4
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-10-15 23:59:41 -0300

    :sparkles: feat: adds mvp/main for testing tokens
    - edit mvp/main_ras_broken.c
    
    - test with main works
    - main with debub funtions

--- mvp/src/main_ras_token.c
+++ mvp/src/main_ras_token.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ras_est_token.c                                    :+:      :+:    :+:   */
+/*   main_ras_token.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lilmende <lilmende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 09:17:16 by lilmende          #+#    #+#             */
-/*   Updated: 2024/10/15 17:47:40 by lilmende         ###   ########.fr       */
+/*   Updated: 2024/10/16 00:22:55 by lilmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,71 @@ int	m_is_special_char(char c)
     return (c == '|' || c == '<' || c == '>');
 }
 
+void	m_skip_whitespace(char *input, int *i)
+{
+	while (input[*i] && ft_isspace(input[*i]))
+		(*i)++;
+}
+
+void	m_process_operator(char *input, int *i, t_token **tokens)
+{
+	if (input[*i] == '|')
+	{
+		m_add_token(tokens, m_create_token("|", PIPE));
+		(*i)++;
+	}
+	else if (input[*i] == '>')
+	{
+		if (input[*i + 1] == '>')
+		{
+			m_add_token(tokens, m_create_token(">>", REDIR_APPEND));
+			(*i) += 2;
+		}
+		else
+		{
+			m_add_token(tokens, m_create_token(">", REDIR_OUT));
+			(*i)++;
+		}
+	}
+	else if (input[*i] == '<')
+	{
+		m_add_token(tokens, m_create_token("<", REDIR_IN));
+		(*i)++;
+	}
+}
+
+void	m_process_word(char *input, int *i, t_token **tokens)
+{
+	char	*word;
+	int		start;
+
+	start = *i;
+	while (input[*i] && !ft_isspace(input[*i]) && !m_is_special_char(input[*i]))
+		(*i)++;
+	word = ft_strndup(&input[start], *i - start);
+	m_add_token(tokens, m_create_token(word, CMD)); // Resolver: diferenciar comando e argumento
+	free(word);
+}
+
+t_token	*m_tokenize(char *input)
+{
+	t_token	*tokens;
+	int		i;
+
+	tokens = NULL;
+	i = 0;
+	while (input[i])
+	{
+		m_skip_whitespace(input, &i);
+		if (input[i] == '|' || input[i] == '>' || input[i] == '<')
+			m_process_operator(input, &i, &tokens);
+		else if (input[i])
+			m_process_word(input, &i, &tokens);
+	}
+	return (tokens);
+}
+
+/*
 t_token	*m_tokenize(char *input)
 {
 	t_token	*tokens;
@@ -99,7 +164,8 @@ t_token	*m_tokenize(char *input)
 		else
 		{
 			start = i;
-			while (input[i] && !ft_isspace(input[i]) && !m_is_special_char(input[i]))
+			while (input[i] && !ft_isspace(input[i]) \
+					&& !m_is_special_char(input[i]))
 				i++;
 			word = ft_strndup(&input[start], i - start);
 			m_add_token(&tokens, m_create_token(word, CMD));
@@ -108,6 +174,7 @@ t_token	*m_tokenize(char *input)
 	}
 	return tokens;
 }
+*/
 
 int main()
 {
