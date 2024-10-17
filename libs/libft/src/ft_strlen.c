1af5bce342f70654871bae542c9d84a361b512af
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-10-17 10:28:37 -0300

    update Libft

--- libs/libft/src/ft_strlen.c
+++ libs/libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lilmende <lilmende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 17:34:27 by lilmende          #+#    #+#             */
-/*   Updated: 2024/09/01 15:15:13 by lilmende         ###   ########.fr       */
+/*   Updated: 2024/10/17 10:27:40 by lilmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 size_t	ft_strlen(const char *s)
 {
-	size_t	count;
+	size_t	len;
 
-	count = 0;
-	while (s[count] != '\0')
-		count++;
-	return (count);
+	len = 0;
+	if (!s)
+		return (0);
+	while (s[len])
+		len++;
+	return (len);
 }
