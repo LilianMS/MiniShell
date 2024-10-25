588ef88e53abe2fd3cd98aec4c3cd782934eaf3e
Letícia <lesampietro@gmail.com>
2024-10-25 16:42:06 -0300

    :sparkles: feat: added m_status_need_backtrack in handle_status file

--- src/handle_status_2.c
+++ src/handle_status_2.c
@@ -24,18 +24,14 @@ int	m_get_status_82(char c)
 		return (-1);
 }
 
-int	m_get_status_83(char c)
+int	m_status_needs_backtrack(int status)
 {
-	if (c == '|')
-		return (100);
-	else if (c == '<')
-		return (100);
-	else if (c == '>')
-		return (100);
-	else if (c == '$')
-		return (100);
-	else
+	if (status == 41 \
+            || status == 62 \
+            || status == 72 \
+            || status == 83)
 		return (1);
+    return (0);
 }
 
 //LEGENDA STATUS
@@ -47,8 +43,8 @@ int	m_get_status_83(char c)
 //---> 71 = REDIR_APPEND --> token type number = 5
 int	m_is_final_status(int status)
 {
-	return (status == 41 || \
-			status == 61 || status == 62 || \
-			status == 71 || status == 72 || \
-			status == 83);
+	return (status == 41 
+                || status == 61 || status == 62 \
+			    || status == 71 || status == 72 \
+			    || status == 83);
 }
