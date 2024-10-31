cf70d852158ad5b942047bf9adb55a41bd3d3677
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-10-31 20:35:09 -0300

    :pencil: draft: creating parsing feature

--- src/minishell.c
+++ src/minishell.c
@@ -15,9 +15,11 @@ void	print_tokens(t_token **tokens)
 
 void	m_lexical_analysis(char *line)
 {
-	t_token	*token_list;
+	t_token *token_list;
+	t_token *parsed_list;
 
 	token_list = NULL;
+	parsed_list = NULL;
 	if (!ft_check_quotes(line))
 	{
 		ft_putendl_fd("minishell: syntax error with open quotes", 2);
@@ -30,11 +32,14 @@ void	m_lexical_analysis(char *line)
 			m_free_tokens(&token_list);
 			return ;
 		}
-		// m_parse_tokens(&token_list); ?
-		// m_execute_commands(&token_list); ?
 		print_tokens(&token_list);
 	}
+	parsed_list = m_parse_tokens(&token_list);
 	m_free_tokens(&token_list);
+	print_tokens(&parsed_list);
+	// m_binary_tree(&parsed_list);
+	// m_execute_commands(&parsed_list); ?
+	m_free_tokens(&parsed_list);
 }
 
 // void	init_minishell(t_mini *mini, char *line)
---
+++ src/parser/parser.c
@@ -0,0 +1,9 @@
+#include "../includes/parser.h"
+
+void m_parse_tokens(t_token **token_list, t_token **parsed_list)
+{
+	//criar função que lê os nós de palavras 
+	//e coloca os valores em um nó só, em um array de arrays
+
+	//criar estrutura com o tipo de nó COMMAND e outros tipos de nós possíveis (VARIABLE, STRING) -> COLOCAR COMMAND, VARIABLE E STRING NOS ENUMS
+}
