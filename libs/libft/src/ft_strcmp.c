e90c2cb1c2b971f59a2c83ecffb83de484969311
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-11-04 19:16:44 -0300

    :sparkles: feat: adds ft_strcmp in Libft
    	- adds ft_strcmp
    	- correct norma in ft_strdup
    	- norminette OK ✅

--- libs/libft/src/ft_strcmp.c
+++ libs/libft/src/ft_strcmp.c
@@ -1,36 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lilmende <lilmende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 13:16:57 by lilmende          #+#    #+#             */
-/*   Updated: 2024/11/04 19:13:36 by lilmende         ###   ########.fr       */
+/*   Created: 2024/11/04 18:38:52 by lilmende          #+#    #+#             */
+/*   Updated: 2024/11/04 18:39:24 by lilmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char	*ft_strdup(const char *s)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	char	*dst;
+	size_t	len1;
+	size_t	len2;
+	size_t	min_len;
+	int		result;
 
-	dst = malloc(ft_strlen (s) + 1);
-	if (dst == NULL)
-		return (NULL);
-	ft_strlcpy(dst, s, ft_strlen (s) + 1);
-	return (dst);
-}
-
-char	*ft_strndup(const char *s, size_t n)
-{
-	char	*dst;
-
-	dst = malloc(n + 1);
-	if (!dst)
-		return (NULL);
-	ft_strlcpy(dst, s, n);
-	dst[n] = '\0';
-	return (dst);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 < len2)
+		min_len = len1;
+	else
+		min_len = len2;
+	result = ft_memcmp(s1, s2, min_len);
+	if (result != 0)
+		return (result);
+	return (len1 - len2);
 }
