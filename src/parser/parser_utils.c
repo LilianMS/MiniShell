5b23ed0a01565fad78276513e112ec6d043be949
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-11-16 15:42:08 -0300

    :sparkles: feat: expands variables
     - include m_clean_dollar
     - obs: need to manage $$ and $n
    
      On branch draft_lilmende
      Changes to be committed:
    	modified:   src/parser/parser_utils.c

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,5 +1,11 @@
 #include "../includes/parser.h"
 
+// função para expadir $$
+pid_t get_pid(void)
+{
+	ft_printf("retona ID do processo atual\n");
+}
+
 // --- funções para m_quotes_and_expansion
 int	m_check_expand(char *lexeme)
 {
@@ -16,14 +22,41 @@ int	m_check_expand(char *lexeme)
 		return (0);
 }
 
+char	*m_clean_dollar(char *str)
+{
+	char	*new_str;
+	int		start;
+	int		end;
+	int		i;
+
+	if (strchr(str, '$'))
+	{
+		new_str = malloc(sizeof(char) * ((ft_strlen(str) - 1) + 1));
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] != '$')
+				*new_str++ = str[i];
+			i++;
+		}
+		*new_str = '\0';
+		new_str -= (ft_strlen(str) - 1);
+		free(str);
+		return (new_str);
+	}
+	return (str);
+}
+
 // --- usando getenv temporariamente
 char	*m_get_expand_string(char *lexeme, t_env *env_list)
 {
 	char	*cleaned_lexeme;
+	char	*temp_cleaned_lexeme;
 	char	*value;
 	char	*empty_string;
 
-	cleaned_lexeme = m_clean_quotes(lexeme);
+	temp_cleaned_lexeme = m_clean_quotes(lexeme);
+	cleaned_lexeme = m_clean_dollar(temp_cleaned_lexeme);
 	value = m_get_env(env_list, cleaned_lexeme);
 	free(cleaned_lexeme);
 	if (value)
