9a69b295c106260468771a179b4145c8d339c826
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-11-18 00:23:58 -0300

    :pencil: draft: no leaks...
     - no leaks
     - works $$, $?, $n, *$* :tada:
     - missing to expand variables in sequence

--- src/parser/expansion_utils.c
+++ src/parser/expansion_utils.c
@@ -6,27 +6,40 @@ pid_t	m_get_pid(void)
 	return (8888);
 }
 
-char	*m_clean_dollar(char *str)
-{
-	char	*new_str;
-	int		i;
-	int		n;
+// char	*m_clean_dollar(char *str)
+// {
+// 	char	*new_str;
+// 	int		i;
+// 	int		n;
 
-	// n = m_qntd_dollar(str);
-	n = 0;
-	i = 0;
-	new_str = malloc(sizeof(char) * ((ft_strlen(str) - 1) + 1));
-	while (str[i])
-	{
-		if (str[i] == '$' && n == 0)
-		{
-			i++;
-			n++;
-		}
-		*new_str++ = str[i];
-		i++;
-	}
-	*new_str = '\0';
-	new_str -= (ft_strlen(str) - 1);
-	return (new_str);
+// 	// n = m_qntd_dollar(str);
+// 	n = 0;
+// 	i = 0;
+// 	new_str = malloc(sizeof(char) * ((ft_strlen(str) - 1) + 1));
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '$' && n == 0)
+// 		{
+// 			i++;
+// 			n++;
+// 		}
+// 		*new_str++ = str[i];
+// 		i++;
+// 	}
+// 	*new_str = '\0';
+// 	new_str -= (ft_strlen(str) - 1);
+// 	return (new_str);
+// }
+
+char	*m_clean_dollar(char *dollar_position)
+{
+	int		len;
+	char	*var_name;
+
+	len = 0;
+	// Avança para depois do $ e captura caracteres válidos para nome de variável
+	while (dollar_position[len + 1] && (ft_isalnum(dollar_position[len + 1]) || dollar_position[len + 1] == '_'))
+		len++;
+	var_name = ft_substr(dollar_position, 1, len); // Pula o $
+	return (var_name);
 }
--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,29 +1,7 @@
 #include "../includes/parser.h"
 
 // --- funções para m_quotes_and_expansion
-int	m_check_expand(char *lexeme)
-{
-	int	str_len;
 
-	str_len = ft_strlen(lexeme);
-	// if ((lexeme[0] == '\"') && (ft_strchr(lexeme, '$'))
-	// 	&& (lexeme[str_len - 1] == '\"')
-	// 	&& (lexeme[2] != '\"') && (lexeme[2] != ' '))
-	// 	return (1);
-	// else if (lexeme[0] == '$' && lexeme[1] != '\0')
-	// 	return (1);
-	// else
-	// 	return (0);
-
-	if ((lexeme[0] == '\"') && (lexeme[1] == '$')
-		&& (lexeme[str_len - 1] == '\"')
-		&& (lexeme[2] != '\"') && (lexeme[2] != ' '))
-		return (1);
-	else if (lexeme[0] == '$' && lexeme[1] != '\0')
-		return (1);
-	else
-		return (0);
-}
 
 // Expansão de `$$`: PID do processo
 		// pid_t	m_get_pid(void);
@@ -33,32 +11,38 @@ int	m_check_expand(char *lexeme)
 
 int	m_is_special_cases_dollar(char *lexeme)
 {
-	if (ft_strncmp(lexeme, "$$", 2) == 0 || ft_strncmp(lexeme, "$?", 2) == 0)
+	char	*dollar_position;
+
+	dollar_position = ft_strchr(lexeme, '$');
+	if (!dollar_position)
+		return (0);
+	if (ft_strncmp(dollar_position, "$$", 2) == 0
+		|| ft_strncmp(dollar_position, "$?", 2) == 0)
 		return (1);
-	if (lexeme[0] == '$' && ft_isdigit(lexeme[1]))
+	if (dollar_position[1] && ft_isdigit(dollar_position[1]))
 		return (1);
 	return (0);
 }
 
-static void	m_expansion_special_cases(char **expansion, char **remaining_text, \
-	char *temp_cleaned_lexeme)
-{
-	if (ft_strncmp(temp_cleaned_lexeme, "$$", 2) == 0)
-	{
-		*expansion = ft_itoa(m_get_pid());
-		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
-	}
-	else if (ft_strncmp(temp_cleaned_lexeme, "$?", 2) == 0)
-	{
-		*expansion = ft_itoa(g_signal_status);
-		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
-	}
-	else if (temp_cleaned_lexeme[0] == '$' && ft_isdigit(temp_cleaned_lexeme[1]))
-	{
-		*expansion = ft_strdup("");
-		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
-	}
-}
+// static void	m_expansion_special_cases(char **expansion, char **remaining_text, \
+// 	char *temp_cleaned_lexeme)
+// {
+// 	if (ft_strncmp(temp_cleaned_lexeme, "$$", 2) == 0)
+// 	{
+// 		*expansion = ft_itoa(m_get_pid());
+// 		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
+// 	}
+// 	else if (ft_strncmp(temp_cleaned_lexeme, "$?", 2) == 0)
+// 	{
+// 		*expansion = ft_itoa(g_signal_status);
+// 		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
+// 	}
+// 	else if (temp_cleaned_lexeme[0] == '$' && ft_isdigit(temp_cleaned_lexeme[1]))
+// 	{
+// 		*expansion = ft_strdup("");
+// 		*remaining_text = ft_strdup(temp_cleaned_lexeme + 2);
+// 	}
+// }
 /*
 int m_is_dollar_outside_single_quotes(char *str, int *index)
 {
@@ -111,41 +95,34 @@ char **m_split_dollar(char *str)
 
 // char	*m_get_expand_string(char *lexeme, t_env *env_list)
 // {
-// 	char	**split_lexeme;
+// 	char	*temp_cleaned_lexeme;
+// 	char	*cleaned_lexeme;
+// 	char	*result;
+// 	char	*expansion;
+// 	char	*remaining_text;
 
-// 	split_lexeme = m_split_dollar(lexeme);
+// 	temp_cleaned_lexeme = m_clean_quotes(lexeme);
+// 	if (m_is_special_cases_dollar(temp_cleaned_lexeme))
+// 		m_expansion_special_cases(&expansion, &remaining_text, temp_cleaned_lexeme);
+// 	else
+// 	{
+// 		cleaned_lexeme = m_clean_dollar(temp_cleaned_lexeme);
+// 		if (m_get_env(env_list, cleaned_lexeme))
+// 			expansion = ft_strdup(m_get_env(env_list, cleaned_lexeme));
+// 		else
+// 			expansion = ft_strdup("");
+// 		free(cleaned_lexeme);
+// 		remaining_text = ft_strdup("");
+// 	}
+// 	if (temp_cleaned_lexeme)
+// 		free(temp_cleaned_lexeme);
+// 	result = ft_strjoin(expansion, remaining_text);
+// 	if (expansion)
+// 		free(expansion);
+// 	if (remaining_text)
+// 		free(remaining_text);
+// 	return (result);
 // }
-
-char	*m_get_expand_string(char *lexeme, t_env *env_list)
-{
-	char	*temp_cleaned_lexeme;
-	char	*cleaned_lexeme;
-	char	*result;
-	char	*expansion;
-	char	*remaining_text;
-
-	temp_cleaned_lexeme = m_clean_quotes(lexeme);
-	if (m_is_special_cases_dollar(temp_cleaned_lexeme))
-		m_expansion_special_cases(&expansion, &remaining_text, temp_cleaned_lexeme);
-	else
-	{
-		cleaned_lexeme = m_clean_dollar(temp_cleaned_lexeme);
-		if (m_get_env(env_list, cleaned_lexeme))
-			expansion = ft_strdup(m_get_env(env_list, cleaned_lexeme));
-		else
-			expansion = ft_strdup("");
-		free(cleaned_lexeme);
-		remaining_text = ft_strdup("");
-	}
-	if (temp_cleaned_lexeme)
-		free(temp_cleaned_lexeme);
-	result = ft_strjoin(expansion, remaining_text);
-	if (expansion)
-		free(expansion);
-	if (remaining_text)
-		free(remaining_text);
-	return (result);
-}
 
 
 // char	*m_get_expand_string(char *lexeme, t_env *env_list)
@@ -202,16 +179,166 @@ int	m_qntd_dollar(char *str)
 	return (n);
 }
 
-char	*m_quotes_and_expansion(char *lexeme, t_env *env_list)
+int	m_check_expand(char *lexeme)
 {
-	char id_quote;
-	int	 n_dollar;
+	char	id_quote;
+	int		n_dollar;
 
 	id_quote = ft_is_qt_type(lexeme);
 	if (ft_strchr(lexeme, '$') && (id_quote == '\"' || id_quote == 0))
 	{
 		n_dollar = m_qntd_dollar(lexeme);
 		if (n_dollar > 1 && !(m_is_special_cases_dollar(lexeme)))
+			return (1);
+		else if ((lexeme[0] == '$' && lexeme[1] == '\0')
+			|| lexeme[2] == '\"' || lexeme[2] == ' ')
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	if (s1)
+		free(s1);
+	return (result);
+}
+
+// static char	*m_expansion_special_cases(char *temp_lexeme)
+// {
+// 	char	*before;
+// 	char	*expansion;
+// 	char	*remaining_text;
+// 	char	*result;
+// 	char	*dollar_position;
+
+// 	dollar_position = ft_strchr(temp_lexeme, '$');
+// 	before = ft_substr(temp_lexeme, 0, dollar_position - temp_lexeme);
+// 	if (ft_strncmp(dollar_position, "$$", 2) == 0)
+// 	{
+// 		expansion = ft_itoa(m_get_pid());
+// 		remaining_text = ft_strdup(dollar_position + 2);
+// 	}
+// 	else if (ft_strncmp(dollar_position, "$?", 2) == 0)
+// 	{
+// 		expansion = ft_itoa(g_signal_status);
+// 		remaining_text = ft_strdup(dollar_position + 2);
+// 	}
+// 	else if (dollar_position[1] && ft_isdigit(dollar_position[1]))
+// 	{
+// 		expansion = ft_strdup("");
+// 		remaining_text = ft_strdup(dollar_position + 2);
+// 	}
+// 	result = ft_strjoin(before, expansion);
+// 	result = ft_strjoin_free(result, remaining_text);
+// 	free(before);
+// 	free(expansion);
+// 	free(remaining_text);
+// 	return (result);
+// }
+
+static char	*m_expansion_special_cases(char *dollar_position)
+{
+	char	*expansion;
+
+	if (ft_strncmp(dollar_position, "$$", 2) == 0)
+		expansion = ft_itoa(m_get_pid());
+	else if (ft_strncmp(dollar_position, "$?", 2) == 0)
+		expansion = ft_itoa(g_signal_status);
+	else if (dollar_position[1] && ft_isdigit(dollar_position[1]))
+		expansion = ft_strdup("");
+	else
+		expansion = ft_strdup("$"); // Caso especial não identificado, retorna o próprio $
+	return (expansion);
+}
+
+char	*m_get_expand_string(char *lexeme, t_env *env_list)
+{
+	char	*temp_cleaned_lexeme;
+	char	*before_dollar;
+	char	*expansion;
+	char	*remaining_text;
+	char	*result;
+	char	*dollar_position;
+	char	*cleaned_lexeme;
+
+	remaining_text = NULL;
+	temp_cleaned_lexeme = m_clean_quotes(lexeme);
+	dollar_position = ft_strchr(temp_cleaned_lexeme, '$');
+	if (!dollar_position) // Caso não haja $, retorna a string limpa
+		return (temp_cleaned_lexeme);
+	before_dollar = ft_substr(temp_cleaned_lexeme, 0, dollar_position - temp_cleaned_lexeme);
+	if (m_is_special_cases_dollar(dollar_position))
+	{
+		expansion = m_expansion_special_cases(dollar_position);
+		remaining_text = ft_strdup(dollar_position + 2);
+	}
+	else
+	{
+		cleaned_lexeme = m_clean_dollar(dollar_position);
+		if (m_get_env(env_list, cleaned_lexeme))
+			expansion = ft_strdup(m_get_env(env_list, cleaned_lexeme));
+		else
+			expansion = ft_strdup("");
+		free(cleaned_lexeme);
+	}
+	if (before_dollar)
+	{
+		result = ft_strjoin(before_dollar, expansion);
+		if (remaining_text)
+			result = ft_strjoin_free(result, remaining_text);
+	}
+	else if(remaining_text)
+	{
+		result = ft_strjoin(expansion, remaining_text);
+	}
+	else
+	{
+		result = ft_strdup(expansion);
+	}
+	free(temp_cleaned_lexeme);
+	free(before_dollar);
+	free(expansion);
+	free(remaining_text);
+	return (result);
+}
+
+
+// char	*m_get_expand_string(char *lexeme, t_env *env_list)
+// {
+// 	char	*temp_cleaned_lexeme;
+// 	char	*cleaned_lexeme;
+// 	char	*result;
+
+// 	temp_cleaned_lexeme = m_clean_quotes(lexeme);
+// 	if (m_is_special_cases_dollar(temp_cleaned_lexeme))
+// 		result = m_expansion_special_cases(temp_cleaned_lexeme);
+// 	else
+// 	{
+// 		cleaned_lexeme = m_clean_dollar(temp_cleaned_lexeme);
+// 		if (m_get_env(env_list, cleaned_lexeme))
+// 			result = ft_strdup(m_get_env(env_list, cleaned_lexeme));
+// 		else
+// 			result = ft_strdup("");
+// 		free(cleaned_lexeme);
+// 	}
+// 	if (temp_cleaned_lexeme)
+// 		free(temp_cleaned_lexeme);
+// 	return (result);
+// }
+
+char	*m_quotes_and_expansion(char *lexeme, t_env *env_list)
+{
+	int	is_expand;
+
+	is_expand = m_check_expand(lexeme);
+	if (is_expand)
+	{
+		if (is_expand == 2)
 			return (m_clean_quotes(lexeme));
 		return (m_get_expand_string(lexeme, env_list));
 	}
