4ca82065ece73526443894af396c98069adf0608
Letícia <lesampietro@gmail.com>
2024-11-23 01:46:01 -0300

    :pencil: draft: first draft to create ast recursion functions

--- includes/ast.h
+++ includes/ast.h
@@ -26,6 +26,6 @@ struct s_tree
 void	m_binary_tree(t_tree *root, t_token **parsed_list);
 t_tree	*m_create_tree_node(t_tree *root, t_token *joint);
 void	m_get_sublist(t_token *rev_list, t_token **parsed_list);
-t_tree	*m_grow_tree(t_tree **root, t_token **joint);
+t_tree	*m_grow_tree(t_tree **root, t_token **joint, t_token **parsed_list);
 
 #endif
--- src/ast/binary_tree.c
+++ src/ast/binary_tree.c
@@ -1,26 +1,38 @@
 #include "../includes/ast.h"
 
-t_tree	*m_grow_tree(t_tree **root, t_token **joint)
+t_tree	*m_grow_tree(t_tree **root, t_token **joint, t_token **parsed_list)
 {
-	*root = m_create_tree_node(*root, *joint);
-	// (*root)->right = m_get_sublist(*rev_list, parsed_list);
-	// m_get_sublist(*rev_list, parsed_list);
-	// (*root)->left = m_get_sublist(*root, *rev_list);
+	t_token	*right;
+	t_token	*left;
+
+	right = NULL;
+	left = parsed_list;
+	if(!*root || !*joint || !*parsed_list)
+		return (NULL);
+	right = (*joint)->next;
+	if(right)
+		right->prev = NULL;
+	(*root)->type = (*joint)->type;
+	left = (*joint)->prev;
+	if (left)
+		left->next = NULL;
+	(*root)->left = m_binary_tree(*root, &left);
+	(*root)->right = m_binary_tree(*root, &right);
 	return (*root);
 }
 
 t_tree	*m_create_tree_node(t_tree *root, t_token *joint)
 {
 	root = ft_calloc(sizeof(t_tree), 1);
-	if (!root)
-		return (NULL);
-	root->type = joint->type;
-	root->index = 0;
-	root->content = NULL;
-	root->command = NULL;
-	root->left = NULL;
-	root->right = NULL;
-	root->parent = NULL;
+	// if (!root)
+	// 	return (NULL);
+	// root->type = joint->type;
+	// root->index = 0;
+	// root->content = NULL;
+	// root->command = NULL;
+	// root->left = NULL;
+	// root->right = NULL;
+	// root->parent = NULL;
 	return (root);
 }
 
@@ -49,17 +61,23 @@ t_token	*m_find_joint_token(t_token	*tokens)
 	return (NULL);
 }
 
-void	m_binary_tree(t_tree *root, t_token **parsed_list)
+t_tree	*m_tree_builder(t_token *parsed_list)
 {
 	t_token	*joint;
+	t_tree	*root;
 
-	joint = m_find_joint_token(*parsed_list);
-	if (joint)
-	{
-		ft_printf("First joint: %s / Type: %d / Address: %p \n", joint->lexeme, joint->type, joint); // ---debug
-		root = m_grow_tree(&root, &joint);
-		list_printer(&joint); // ----- debug
-		ft_printf("Tree root: %s / Type: %d / Address: %p \n", root->content, root->type, root); // ----- debug
+	joint = m_find_joint_token(parsed_list);
+	// ft_printf("First joint: %s / Type: %d / Address: %p \n", joint->lexeme, joint->type, joint); // ---debug
+	root = ft_calloc(sizeof(t_tree), 1);
+	m_grow_tree(&root, &joint, parsed_list);
+	// list_printer(&joint); // ----- debug
+	ft_printf("Tree root: %s / Type: %d / Address: %p \n", root->content, root->type, root); // ----- debug
+	if (root)	
 		free(root);
-	}
+
+}
+
+void	m_binary_tree(t_tree *root, t_token **parsed_list)
+{
+	root = m_tree_builder(*parsed_list);
 }
