3ce5549777d6ed3737acf7cb5cb2812ae338ae87
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-11-27 13:29:47 -0300

    :sparkles: feat: improves input line checker #63
     - manages invalid characters when outside the quotes.
     - resolves #63 :tada:

---
+++ src/utils/checkers.c
@@ -0,0 +1,63 @@
+#include "../../includes/minishell.h"
+
+static int	ft_check_quotes(const char *line)
+{
+	int	single_q;
+	int	double_q;
+
+	single_q = 0;
+	double_q = 0;
+	while (*line)
+	{
+		if (*line == '\'' && !double_q)
+			single_q = !single_q;
+		else if (*line == '\"' && !single_q)
+			double_q = !double_q;
+		line++;
+	}
+	return (!single_q && !double_q);
+}
+
+int	is_quoted(char c, char *quote)
+{
+	if (*quote == '\0' && (c == '\'' || c == '\"'))
+		*quote = c; // Inicia um contexto de aspas
+	else if (*quote == c)
+		*quote = '\0'; // Fecha o contexto de aspas
+	return (*quote != '\0'); // Retorna se estamos dentro de aspas
+}
+
+int	check_invalid_chars(const char *input)
+{
+	const char	*invalid_chars = "();\\";
+	char		quote;
+
+	quote = '\0'; // Rastreamos se estamos dentro de aspas
+	while (*input)
+	{
+		if (is_quoted(*input, &quote))
+		{
+			input++; // Ignore os caracteres dentro das aspas
+			continue ;
+		}
+		if (ft_strchr(invalid_chars, *input))
+			return (1); // Encontrou um caractere inválido fora das aspas
+		input++;
+	}
+	return (0); // Nenhum caractere inválido fora das aspas
+}
+
+int	m_check_line_input(const char *line)
+{
+	if (!ft_check_quotes(line))
+	{
+		ft_putendl_fd("minishell: syntax error with open quotes", 2);
+		return (0);
+	}
+	if (check_invalid_chars(line))
+	{
+		ft_putendl_fd("minishell: syntax error with invalid characters", 2);
+		return (0);
+	}
+	return (1);
+}
--- src/utils/cleaners.c
+++ src/utils/cleaners.c
@@ -1,51 +1,5 @@
 #include "../../includes/minishell.h"
 
-static int	ft_check_quotes(const char *line)
-{
-	int	single_q;
-	int	double_q;
-
-	single_q = 0;
-	double_q = 0;
-	while (*line)
-	{
-		if (*line == '\'' && !double_q)
-			single_q = !single_q;
-		else if (*line == '\"' && !single_q)
-			double_q = !double_q;
-		line++;
-	}
-	return (!single_q && !double_q);
-}
-
-static int	check_invalid_chars(const char *line)
-{
-	const char	*invalid_chars = "();\\";
-
-	while (*invalid_chars)
-	{
-		if (ft_strchr(line, *invalid_chars))
-			return (1);
-		invalid_chars++;
-	}
-	return (0);
-}
-
-int	m_check_line_input(const char *line)
-{
-	if (!ft_check_quotes(line))
-	{
-		ft_putendl_fd("minishell: syntax error with open quotes", 2);
-		return (0);
-	}
-	if (check_invalid_chars(line))
-	{
-		ft_putendl_fd("minishell: syntax error with invalid characters", 2);
-		return (0);
-	}
-	return (1);
-}
-
 char	ft_is_quotes_type(char *lexeme, int *start, int *end)
 {
 	char	id_quote;
