afc8520977ef752e328ecf1f69d652b0ee2a6b63
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-12-06 16:07:41 -0300

    :pencil: draft: finds last arg pointer

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -112,37 +112,54 @@ static void	m_add_post_redir_type(t_token **token_list)
 // FAZ ISSO RESURSIVAMENTE COM TODOS OS NÓS QUE ATENDAM O REQUISITO
 //ACHOU UM PIPE, PÁRA E RETORNA UM POINTER PARA O PRÓXIMO NÓ (APÓS PIPE), QUE DEVERÁ SER UM COMANDO, E PROCURARÁ NOVAMENTE NA FUNÇÃO RECURSIVA O TARGET NODE, INICIANDO O CICLO NOVAMENTE, ATÉ A LISTA ACABAR.
 
-// static void	m_pre_process(t_token **token_list)
+// static void	m_relocate_word_node(t_token **token_list, t_token **first_word)
 // {
-// 	t_token	*aux_list;
-// 	t_token	*first_word;
-// 	t_token	*word_sublist;
+// 	t_token	*target_node;
 
-// 	aux_list = *token_list;
-// 	first_word = NULL;
-// 	if (aux_list->type == WORD)
-// 		first_word = aux_list;
-// 	while (aux_list)
+// 	target_node = NULL;
+// 	while (*token_list && (*token_list)->type != PIPE)
 // 	{
-// 		while (aux_list && !m_is_redir(aux_list->type))
-// 			aux_list = aux_list->next;
-// 		if (m_is_redir(aux_list))
-// 			aux_list = aux_list->next;
-// 		if (aux_list->type == WORD)
-// 			word_sublist = aux_list;
 
+// 		m_relocate_word_node()
 // 	}
-// 	return (aux_list);
 // }
+
+static t_token	*m_find_last_arg(t_token *token_list)
+{
+	t_token	*last_arg;
+
+	last_arg = NULL;
+	while (token_list && token_list->type == WORD)
+	{
+		last_arg = token_list;
+		token_list = token_list->next;
+	}
+	return (last_arg);
+}
+
+static void	m_pre_process(t_token **token_list)
+{
+	t_token	*front_node;
+	t_token	*word_sublist;
+
+	front_node = *token_list;
+	if ((*token_list)->type == WORD)
+		front_node = m_find_last_arg(*token_list);
+	ft_printf("\nLast arg = %s\nToken type = %i\nNext token type = %i\n", front_node->lexeme, front_node->type, front_node->next->type);
+	// while (aux_list)
+	// {
+	// }
+	// return (aux_list);
+}
 
 t_token	*m_parse_tokens(t_token **token_list, t_token **parsed_list, \
 						t_env *env_list)
 {
 	t_token	*aux_list;
 
-	m_add_post_redir_type(token_list);
+	m_add_post_redir_type(token_list); //adiciona os tipos DELIMITER e FILENAME nos nós, diretamente na token_list -> precisaremos disso para a m_pre_process
 	aux_list = *token_list;
-	// m_pre_process(aux_list); // reorganiza os nós antes de mallocar os novos nós de comando
+	m_pre_process(&aux_list); // reorganiza os nós antes de mallocar os novos nós de comando
 	while (aux_list)
 	{
 		if (aux_list->type == WORD)
@@ -157,23 +174,3 @@ t_token	*m_parse_tokens(t_token **token_list, t_token **parsed_list, \
 	}
 	return (*parsed_list);
 }
-
-// versão antiga
-
-// t_token	*m_parse_tokens(t_token **token_list, t_token **parsed_list, t_env *env_list)
-// {
-// 	t_token	*aux_list;
-
-// 	aux_list = *token_list;
-// 	while (aux_list)
-// 	{
-// 		if (aux_list->type == WORD)
-// 			m_handle_word_tokens(&aux_list, parsed_list, env_list);
-// 		else
-// 		{
-// 			m_copy_token(parsed_list, aux_list);
-// 			aux_list = aux_list->next;
-// 		}
-// 	}
-// 	return (*parsed_list);
-// }
