045d7e8d8cb8d4d0741294838d537ea35a1458c9
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-12-11 06:07:25 -0300

    :pencil: draft: fix pipe and redir_in

--- src/tokenizer/token_validation.c
+++ src/tokenizer/token_validation.c
@@ -1,5 +1,33 @@
 #include "../includes/tokenizer.h"
 
+/*
+int	m_validate_tokens(t_token *tokens)
+{
+	t_token	*current;
+
+	current = tokens;
+	while (current != NULL)
+	{
+		if ((m_is_redir(current->type) \
+			&& (current->next == NULL || current->next->type != WORD)) \
+			|| (current->type == PIPE \
+			&& (current->next == NULL \
+			|| (current->next->type != WORD && current->next->type != REDIR_IN))))
+		{
+				ft_putendl_fd("Error: Expected a word or input redirection after the pipe.", 2);
+				return (0);
+		}
+		if (current->type == PIPE && (current->prev == NULL || current->prev->type != WORD))
+		{
+			ft_putendl_fd("Error: Expected a word before the operator.", 2);
+			return (0);
+		}
+		current = current->next;
+	}
+	return (1);
+}
+*/
+
 int	m_validate_tokens(t_token *tokens)
 {
 	t_token	*current;
