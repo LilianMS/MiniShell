5c541b5cd22e395e0f450f9b54ad94522f0642b2
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-12-26 11:55:35 -0300

    update: free  heredoc in exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,6 +6,8 @@ int	m_exit(t_mini **mini, t_token **parsed_list)
 	m_free_env_list((*mini)->env_list);
 	m_free_tokens(parsed_list);
 	m_tree_cleaner((*mini)->tree);
+	m_heredoc_delete_files(*mini);
+	free((*mini)->hdoc);
 	rl_clear_history();
 	exit(0);
 }
