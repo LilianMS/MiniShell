25c3b2163682c91f877bd780a1b0e14dec608be1
Letícia <lesampietro@gmail.com>
2025-01-01 20:59:06 -0300

    :pencil: draft:no infinite loop but still not working properly

--- includes/execution.h
+++ includes/execution.h
@@ -11,6 +11,14 @@
 //Structs
 typedef struct s_mini		t_mini;
 typedef struct s_token		t_token;
+typedef struct s_redir		t_redir;
+
+// Estrutura para guardar os file descriptors originais
+struct s_redir {
+    int stdin_backup;
+    int stdout_backup;
+    int stderr_backup;
+};
 
 //Functions
 void     m_execution(t_mini *mini, t_token **parsed_list);
@@ -18,5 +26,8 @@ int		m_execute_command(char **tree_node_cmd, t_env *env_list);
 char	*m_create_path(char *cmd_path, char **node_cmd, char **envp);
 char	**m_env_list_to_array(t_env *env_list);
 int		m_check_cmd(char *cmd_path);
+int     m_execute_redir(t_mini *mini, t_redir *redir_fd);
+void    m_init_redirect(t_redir *redir_fd);
+void    m_restore_redirect(t_redir *redir_fd);
 
 #endif
--- src/execution/execution.c
+++ src/execution/execution.c
@@ -1,5 +1,31 @@
 #include "../../includes/minishell.h"
 
+// Função para inicializar os backups dos fds
+void m_init_redirect(t_redir *redir_fd)
+{
+    redir_fd->stdin_backup = dup(STDIN_FILENO);
+    redir_fd->stdout_backup = dup(STDOUT_FILENO);
+    redir_fd->stderr_backup = dup(STDERR_FILENO);
+}
+
+// Função para restaurar os fds originais
+void m_restore_redirect(t_redir *redir_fd)
+{
+    dup2(redir_fd->stdin_backup, STDIN_FILENO);
+    dup2(redir_fd->stdout_backup, STDOUT_FILENO);
+    dup2(redir_fd->stderr_backup, STDERR_FILENO);
+    close(redir_fd->stdin_backup);
+    close(redir_fd->stdout_backup);
+    close(redir_fd->stderr_backup);
+}
+
+void	m_free_everything(t_mini *mini, t_token **parsed_list)
+{
+	m_free_env_list(mini->env_list);
+	m_free_tokens(parsed_list);
+	m_tree_cleaner(mini->tree);
+}
+
 int m_check_cmd(char *cmd_path)
 {
 	int exit_status;
@@ -37,34 +63,39 @@ int m_execute_command(char **tree_node_cmd, t_env *env_list)
 int	m_simple_command(t_mini *mini, t_token **parsed_list)
 {
 	int pid;
-	int exit_status;
+	t_redir redir_fd;
 
 	pid = 0;
-	exit_status = 0;
 	if (mini->tree->type == COMMAND)
 	{
-		if (m_is_builtin(mini->tree) != -1)
-			exit_status = m_execute_builtin(mini, parsed_list);
-		else
+		if (m_is_builtin(mini->tree) != -1) {
 		{
-			pid = fork();
-			if (pid == 0)
+			if (m_execute_redir(mini, &redir_fd)) 
+				mini->exit_status = 1;
+			else 
 			{
-				exit_status = m_execute_command(mini->tree->command, mini->env_list);
-				// TRANSFORMAR AS LINHAS ABAIXO EM UMA FUNÇÃO ÚNICA
-				m_free_env_list(mini->env_list);
-				m_free_tokens(parsed_list);
-				m_tree_cleaner(mini->tree);
-				exit(mini->exit_status);
-			}
-			else
-			{
-				waitpid(pid, &mini->exit_status, 0);
-				exit_status = WEXITSTATUS(mini->exit_status);
+				mini->exit_status = m_execute_builtin(mini, parsed_list);
+				m_restore_redirect(&redir_fd);
 			}
 		}
 	}
-	return (exit_status);
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			mini->exit_status = m_execute_command(mini->tree->command, mini->env_list);
+			m_free_everything(mini, parsed_list);
+			exit(mini->exit_status);
+		}
+		else
+		{
+			waitpid(pid, &mini->exit_status, 0);
+			mini->exit_status = WEXITSTATUS(mini->exit_status);
+		}
+	}
+	}
+	return (mini->exit_status);
 }
 
 int	m_close_fd(int fd)
@@ -74,44 +105,26 @@ int	m_close_fd(int fd)
 	return (-1);
 }
 
-// int	m_execute_command_2(t_tree *tree, t_minishell *data)
-// {
-// 	int		args_num;
-// 	int		ret_code;
-
-// 	ret_code = 0;
-// 	args_num = token_list_size(tree->command);
-// 	if (args_num != 0)
-// 	{
-// 		ret_code = exec_execve(tree, data);
-// 		close(STDIN_FILENO);
-// 		close(STDOUT_FILENO);
-// 		close(STDERR_FILENO);
-// 	}
-// 	return (ret_code);
-// }
-
-int	m_execute_redir(t_mini *mini)
+int	m_execute_redir(t_mini *mini, t_redir *redir_fd)
 {
-	int		exit_status;
-
-	exit_status = 0; //mudar para receber o código certo
-	ft_printf("filename = %s\n\n", mini->tree->right->content);
+	m_init_redirect(redir_fd);
 	if (mini->tree->type == REDIR_OUT)
 		mini->fd = open(mini->tree->right->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else
-		mini->fd = -1;
 	if (mini->fd == -1)
 	{
-		perror(mini->tree->right->content);
+		perror("minishell: output redirection");
 		return (1);
 	}
-	dup2(mini->fd, STDOUT_FILENO);
-	mini->fd = m_close_fd(mini->fd);
+	if(dup2(mini->fd, STDOUT_FILENO) == -1)
+	{
+		perror("minishell: dup2 error");
+		close(mini->fd);
+		return (1);
+	}
 	// close(STDIN_FILENO);
-	close(STDOUT_FILENO);
+	close(mini->fd);
 	// close(STDERR_FILENO);
-	return (exit_status);
+	return (0);
 }
 
 void	m_execution(t_mini *mini, t_token **parsed_list)
@@ -123,10 +136,10 @@ void	m_execution(t_mini *mini, t_token **parsed_list)
 		&& mini->tree->left == NULL \
 		&& mini->tree->right == NULL)
 		mini->exit_status = m_simple_command(mini, parsed_list);
-	else if (mini->tree->type == REDIR_OUT \
-		&& mini->tree->left->type == COMMAND \
-		&& mini->tree->right->type == FILENAME)
-		mini->exit_status = m_execute_redir(mini);
+	// else if (mini->tree->type == REDIR_OUT
+	// 	&& mini->tree->left->type == COMMAND
+	// 	&& mini->tree->right->type == FILENAME)
+	// 	mini->exit_status = m_execute_redir(mini);
 	m_tree_cleaner(mini->tree);
 	// m_free_env_list(mini->env_list);
 	m_free_tokens(parsed_list);
