fcf0d175bb089634d3a93c916047b5bca7dc6f86
Letícia Sampietro <lsampiet@student.42sp.org.br>
2025-01-08 19:55:59 -0300

    :pencil: draft: multiple redirection working - need to check fds closure

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -96,10 +96,10 @@ int	m_close_fd(int fd)
 	return (1);
 }
 
-int	m_execute_redir_in(t_mini *mini, t_redir *redir_fd)
+int m_execute_redir_in(t_tree *current, t_redir *redir_fd)
 {
-	if (mini->tree->type == REDIR_IN)
-		redir_fd->current_fd = open(mini->tree->right->content, O_RDONLY);
+	if (current->type == REDIR_IN)
+		redir_fd->current_fd = open(current->right->content, O_RDONLY);
 	if (redir_fd->current_fd == -1)
 	{
 		perror("minishell: input redirection");
@@ -108,8 +108,8 @@ int	m_execute_redir_in(t_mini *mini, t_redir *redir_fd)
 	}
 	if(dup2(redir_fd->current_fd, STDIN_FILENO) == -1)
 	{
-		ft_putnbr_fd(redir_fd->current_fd, STDERR_FILENO);
-		ft_putendl_fd(mini->tree->right->content, STDERR_FILENO);
+		ft_putnbr_fd(redir_fd->current_fd, STDERR_FILENO); // -->debug
+		ft_putendl_fd(current->right->content, STDERR_FILENO); // -->debug
 		perror("minishell: dup2 error");
 		close(redir_fd->current_fd);
 		m_restore_redirect(redir_fd);
@@ -119,12 +119,15 @@ int	m_execute_redir_in(t_mini *mini, t_redir *redir_fd)
 	return (0);
 }
 
-int	m_execute_redir_out_append(t_mini *mini, t_redir *redir_fd)
+int	m_execute_redir_out_append(t_tree *current, t_redir *redir_fd)
 {
-	if (mini->tree->type == REDIR_OUT)
-		redir_fd->current_fd = open(mini->tree->right->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else if (mini->tree->type == REDIR_APPEND)
-		redir_fd->current_fd = open(mini->tree->right->content, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (current->type == REDIR_OUT)
+	{
+		redir_fd->current_fd = open(current->right->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		ft_putendl_fd(current->right->content, STDERR_FILENO); // -->debug
+	}
+	else if (current->type == REDIR_APPEND)
+		redir_fd->current_fd = open(current->right->content, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (redir_fd->current_fd == -1)
 	{
 		perror("minishell: output redirection");
@@ -133,8 +136,8 @@ int	m_execute_redir_out_append(t_mini *mini, t_redir *redir_fd)
 	}
 	if(dup2(redir_fd->current_fd, STDOUT_FILENO) == -1)
 	{
-		ft_putnbr_fd(redir_fd->current_fd, STDERR_FILENO);
-		ft_putendl_fd(" OUT", STDERR_FILENO);
+		ft_putnbr_fd(redir_fd->current_fd, STDERR_FILENO); // -->debug
+		ft_putendl_fd(" OUT", STDERR_FILENO);			   // -->debug
 		perror("minishell: dup2 error");
 		close(redir_fd->current_fd);
 		m_restore_redirect(redir_fd);
@@ -151,13 +154,13 @@ t_tree	*m_find_command_node(t_tree *node)
 	current = node;
 	if (current->left)
 	{
-		while (current && current->type != COMMAND)
-				current = current->left;
+		while (current->left && current->type != COMMAND)
+			current = current->left;
 	}
 	return (current);
 }
 
-int	m_execute_all_redirs(t_mini *mini, t_redir *redir_fd, t_tree *node)
+int	m_execute_all_redirs(t_redir *redir_fd, t_tree *node)
 {
 	t_tree *current;
 
@@ -165,18 +168,17 @@ int	m_execute_all_redirs(t_mini *mini, t_redir *redir_fd, t_tree *node)
 	current = m_find_command_node(node);
 	if (current->type == COMMAND)
 		current = current->parent;
-	current = node;
 	while (current && m_is_redir(current->type))
 	{
 		//ALTERAR HEREDOC PARA ACEITAR T_TREE AO INVÉS DE T_TOKEN
 		// if (mini->tree->type == REDIR_HEREDOC)
 		// 	m_heredoc(parsed_list);
 		if (current->type == REDIR_IN)
-			m_execute_redir_in(mini, redir_fd);
+			m_execute_redir_in(current, redir_fd);
 		else if (current->type == REDIR_OUT)
-			m_execute_redir_out_append(mini, redir_fd);
+			m_execute_redir_out_append(current, redir_fd);
 		else if (current->type == REDIR_APPEND)
-			m_execute_redir_out_append(mini, redir_fd);
+			m_execute_redir_out_append(current, redir_fd);
 		current = current->parent;
 	}
 	return (0);
@@ -190,7 +192,7 @@ int	m_handle_redir(t_mini *mini, t_redir *redir_fd, t_token **parsed_list)
 
 	status = 0;
 	original_root = mini->tree;
-	if (m_execute_all_redirs(mini, redir_fd, mini->tree))
+	if (m_execute_all_redirs(redir_fd, mini->tree))
 		return (1);
 	cmd_node = m_find_command_node(mini->tree);
 	if (!cmd_node)
