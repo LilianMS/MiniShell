f1964afe770dc1d14302832f99f07527eb4b4d39
LilianMS <lilian.hblf@gmail.com>
2025-01-25 11:14:54 -0300

    fix: exit builtin #110

--- includes/builtin.h
+++ includes/builtin.h
@@ -4,6 +4,8 @@
 //Libs
 # include "minishell.h"
 
+# define INT_MIN -2147483648
+# define INT_MAX  2147483647
 # ifndef PATH_MAX
 #  define PATH_MAX 4096
 # endif
@@ -27,7 +29,7 @@ int		ft_echo(char **args);
 int		m_env(t_env *env_list);
 int		m_export(t_env *env_list, char **args);
 int		m_unset(t_env *env_list, char **args);
-int		m_exit(t_mini **mini);
+int		m_exit(t_mini **mini, char **args);
 int		m_is_builtin(t_tree *tree_node);
 int		m_execute_builtin(t_tree *node, t_mini *mini);
 void	m_print_sorted_env(t_env *env_list);
